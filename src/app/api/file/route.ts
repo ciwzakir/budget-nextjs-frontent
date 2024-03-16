0adb1db52c7be2cf44ab3f0ec67f8cdb3ec0c8be
ciwzakir <zenithciw.com>
2024-03-16 14:03:59 +0600

    first codfdfdfs

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,6 +1,12 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+// export async function POST(req: NextApiRequest, res: NextApiResponse) {
+//   return NextResponse.json({ name: "File Uploaded" });
+// }
+
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function handler(req: NextRequest, res: NextResponse) {
   return NextResponse.json({ name: "File Uploaded" });
 }
