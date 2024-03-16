a05d0e0b848b6d0112957e0ec70d421ce7151cb3
ciwzakir <zenithciw.com>
2024-03-16 14:40:47 +0600

    pp

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,12 +1,6 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-// export async function POST(req: NextApiRequest, res: NextApiResponse) {
-//   return NextResponse.json({ name: "File Uploaded" });
-// }
-
-import { NextRequest, NextResponse } from "next/server";
-
-export default async function POST(req: NextRequest, res: NextResponse) {
-  return NextResponse.json({ name: "File Uploaded" });
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  return NextResponse.json({ name: "File uploaded" });
 }
